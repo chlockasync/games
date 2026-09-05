8113650bbf218f425d4b89f85ac17651839cb9f6
chlockasync <chlockasync@gmail.com>
2026-09-05 13:53:18 +1200

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE='each-combined-v7';
+const CACHE='each-combined-v10';
 const SHELL=['./each.html','./one.html','./two.html','./manifest.webmanifest','./icons/favicon-32.png','./icons/favicon.ico','./icons/apple-touch-icon.png','./icons/icon-192.png','./icons/icon-512.png','./icons/icon-maskable-512.png'];
 self.addEventListener('install',event=>event.waitUntil(caches.open(CACHE).then(c=>c.addAll(SHELL)).then(()=>self.skipWaiting())));
 self.addEventListener('activate',event=>event.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k)))).then(()=>self.clients.claim())));
